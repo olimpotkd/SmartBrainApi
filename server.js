553cfb52af8a8bdcb7a0b1a8d821f1b6be7b4fc5
Carlos Reynoso <olimpotkd@yahoo.com>
2019-08-06 23:49:56 -0400

    Added cors npm package

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors()); 
 
 
 const database = {
@@ -31,7 +34,6 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-  console.log(req.body);
   if(req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password) {
       res.json('success')
