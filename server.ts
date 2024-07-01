fb23eb4c2d7a3c2160b1951e84aaf466a017504c
Carlos Reynoso <olimpotkd@gmail.com>
2024-07-01 19:15:50 -0400

    Typescript fixes

--- server.ts
+++ server.ts
@@ -1,6 +1,4 @@
-import express from "express";
-import bodyParser from "body-parser";
-// import cors from "cors";
+import express, { Request, Response } from "express";
 import knex from "knex";
 import * as register from "./controllers/register";
 import * as profile from "./controllers/profile";
@@ -22,29 +20,30 @@ const db = knex({
 });
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
+// app.use(bodyParser.json());
 // app.use(cors());
 
-app.post("/signin", (req, res) => {
+app.post("/signin", (req: Request, res: Response) => {
   signin.handleSignIn(req, res, db);
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", (req: Request, res: Response) => {
   register.handleRegister(req, res, db);
 });
 
-app.get("/profile/:id", (req, res) => {
+app.get("/profile/:id", (req: Request, res: Response) => {
   profile.getProfileById(req, res, db);
 });
 
-app.put("/image", (req, res) => {
+app.put("/image", (req: Request, res: Response) => {
   image.updateEntries(req, res, db);
 });
-app.post("/imageUrl", (req, res) => {
+app.post("/imageUrl", (req: Request, res: Response) => {
   image.handleAPICall(req, res);
 });
 
-const port = process.env.PORT;
+const port = <string>process.env.PORT;
 app.listen(port, () => {
   console.log("Server running on port ", port);
 });
